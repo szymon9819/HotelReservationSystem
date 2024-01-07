4ac98137f8d73ac3e25a8fa7c47ff791f4043bf1
Szymon Nowakowski <szymon9819@gmail.com>
2024-01-07 19:18:53 +0100

    feat(db-structure): Update recvtor rule

--- Backend/payment-service/rector.php
+++ Backend/payment-service/rector.php
@@ -21,7 +21,7 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82,
+        LevelSetList::UP_TO_PHP_83,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::TYPE_DECLARATION,
